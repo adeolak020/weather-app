d9626edc1d63a901bf13dd0f324021198906141e
adeolak020 <134192496+adeolak020@users.noreply.github.com>
2023-05-21 20:01:13 -0400

    Created the index.js

---
+++ index.js
@@ -0,0 +1,69 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+function showTemperature(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#current-temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+
+function searchEngine(city) {
+  let units = "metric";
+  let apiKey = "8d9838178b5b401f1b4e7cb5af18e210";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function search(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchEngine(city);
+}
+
+function showCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  let units = "metric";
+  let apiKey = "8d9838178b5b401f1b4e7cb5af18e210";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(showTemperature);
+}
+
+let dateElement = document.querySelector("#date");
+let now = new Date();
+let searchForm = document.querySelector("#search-form");
+let searching = document.querySelector("#searching");
+
+searchForm.addEventListener("submit", search);
+searching.addEventListener("click", showCity);
+
+dateElement.innerHTML = formatDate(now);
+
+searchEngine("Lagos");
