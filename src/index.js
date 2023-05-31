874c83e19ff8f7e48e708e52854049516b6521e7
adeolak020 <134192496+adeolak020@users.noreply.github.com>
2023-05-31 14:40:05 -0400

    Updated the icon API

--- src/index.js
+++ src/index.js
@@ -24,6 +24,7 @@ function formatDate(date) {
 }
 
 function showTemperature(response) {
+  console.log(response.data);
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#current-temp").innerHTML = Math.round(
     response.data.main.temp
@@ -31,9 +32,17 @@ function showTemperature(response) {
   let descriptionelement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let iconElement = document.querySelector("#icon");
+
   descriptionelement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchEngine(city) {
