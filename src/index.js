f08beaf4a4edd1390bb0ea22d505d04c4670b09c
adeolak020 <134192496+adeolak020@users.noreply.github.com>
2023-06-25 16:01:55 -0400

    Updating my columns

--- src/index.js
+++ src/index.js
@@ -23,8 +23,72 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="card">
+  <div class="card-body">`;
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6)
+      forecastHTML =
+        forecastHTML +
+        `
+                  <div class="container text-center">
+                    <div class="row justify-content-start">
+                      <div class="col-4">
+                        <small
+                          ><span class="weather-forecast-date">${formatDay(
+                            forecastDay.dt
+                          )}</span> <br />
+                          04/17</small
+                        >
+                      </div>
+                    </div>
+                    <div class="row justify-content-end">
+                      <div class="weather-cast"><span class="col-4 tempForecast-max">${Math.round(
+                        forecastDay.temp.max
+                      )}°</span><span class="weather-forecast-min"> ${Math.round(
+          forecastDay.temp.min
+        )}°</span></div>
+                      <div class="col-4 forecast"> 
+                      <img src="https://openweathermap.org/img/wn/${
+                        forecastDay.weather[0].icon
+                      }@2x.png"
+                        alt=""
+                        width="42" /></div>
+                      <hr />
+                    </div>
+                    <div class="row justify-content-start">
+                      <div class="col-4">
+                  </div>
+                </div>
+              </div>
+`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let units = "imperial";
+  let apiKey = "7d478f69e1b2f5d563653f13f5f91d76";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showTemperature(response) {
-  console.log(response.data);
+  console.log(response);
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#current-temp").innerHTML = Math.round(
     response.data.main.temp
@@ -43,88 +107,13 @@ function showTemperature(response) {
   );
 
   iconElement.setAttribute("alt", response.data.weather[0].description);
-}
 
-function displayForecast() {
-  let forecastElement = document.querySelector("#forecast");
-
-  forecastElement.innerHTML = `
-              <div class="card">
-                <div class="card-body">
-                  <div class="container text-center">
-                    <div class="row justify-content-start">
-                      <div class="col-4">
-                        <small
-                          >MON <br />
-                          04/17</small
-                        >
-                      </div>
-                    </div>
-                    <div class="row justify-content-end">
-                      <div class="col-4">67°</div>
-                      <div class="col-4 forecast">🌧</div>
-                      <hr />
-                    </div>
-                    <div class="row justify-content-start">
-                      <div class="col-4">
-                        <small
-                          >TUE <br />
-                          04/18</small
-                        >
-                      </div>
-                    </div>
-                    <div class="row justify-content-end">
-                      <div class="col-4">75°</div>
-                      <div class="col-4 forecast">🌦</div>
-                      <hr />
-                    </div>
-                    <div class="row justify-content-start">
-                      <div class="col-4">
-                        <small
-                          >WED <br />
-                          04/19</small
-                        >
-                      </div>
-                    </div>
-                    <div class="row justify-content-end">
-                      <div class="col-4">83°</div>
-                      <div class="col-4 forecast">🌥</div>
-                      <hr />
-                    </div>
-                    <div class="row justify-content-start">
-                      <div class="col-4">
-                        <small
-                          >THU <br />
-                          04/20</small
-                        >
-                      </div>
-                    </div>
-                    <div class="row justify-content-end">
-                      <div class="col-4">86°</div>
-                      <div class="col-4 forecast">🌤</div>
-                      <hr />
-                    </div>
-                    <div class="row justify-content-start">
-                      <div class="col-4">
-                        <small
-                          >FRI <br />
-                          04/21</small
-                        >
-                      </div>
-                    </div>
-                    <div class="row justify-content-end">
-                      <div class="col-4">82°</div>
-                      <div class="col-4 forecast">🌤</div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-`;
+  getForecast(response.data.coord);
 }
 
 function searchEngine(city) {
-  let units = "metric";
-  let apiKey = "8d9838178b5b401f1b4e7cb5af18e210";
+  let units = "imperial";
+  let apiKey = "aa09763d916df0424c840d55bfc2d2c9";
   let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
   let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=${units}`;
 
@@ -136,8 +125,6 @@ function search(event) {
   let city = document.querySelector("#city-input").value;
   searchEngine(city);
 }
-
-displayForecast();
 
 let dateElement = document.querySelector("#date");
 let now = new Date();
